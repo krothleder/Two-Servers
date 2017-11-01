779dad4a2c699d68011a7c4024b95aab6f203c1d
Kaylynn <kaylynn.rothleder@gmail.com>
2017-10-31 19:24:33 -0700

    second commit

--- two_servers.js
+++ two_servers.js
@@ -5,21 +5,21 @@ var PORT_1 = 7000;
 var PORT_2 = 7500;
 
 function handleRequest1(request, response) {
-    response.end("It Works!!!! Path hit: " + request.url);
+    response.end("You are Awesome!" );
 }
 
 
-function handleRequest1(request, response) {
-    response.end("It Works!!!! Path hit: " + request.url);
+function handleRequest2(request, response) {
+    response.end("You suck!");
 }
 
 var server1 = http.createServer(handleRequest1);
 var server2 = http.createServer(handleRequest2);
 
-server.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:%s", PORT);
+server1.listen(PORT_1, function() {
+    console.log("Server listening on: http://localhost:%s", PORT_1);
 });
 
-server.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:%s", PORT);
+server2.listen(PORT_2, function() {
+    console.log("Server listening on: http://localhost:%s", PORT_2);
 });
